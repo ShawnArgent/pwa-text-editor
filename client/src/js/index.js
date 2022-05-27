cc08b88b27789d8844144e967a3f48f916ac92ce
Shawn Argent <kshawn001@gmail.com>
2022-05-26 20:51:19 -0700

    ready to deploy?

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,13 +1,13 @@
-import Editor from "./editor";
-import "./database";
-import "../css/style.css";
+import Editor from './editor';
+import './database';
+import '../css/style.css';
 
-const main = document.querySelector("#main");
-main.innerHTML = "";
+const main = document.querySelector('#main');
+main.innerHTML = '';
 
 const loadSpinner = () => {
-  const spinner = document.createElement("div");
-  spinner.classList.add("spinner");
+  const spinner = document.createElement('div');
+  spinner.classList.add('spinner');
   spinner.innerHTML = `
   <div class="loading-container">
   <div class="loading-spinner" />
@@ -18,14 +18,14 @@ const loadSpinner = () => {
 
 const editor = new Editor();
 
-if (typeof editor === "undefined") {
+if (typeof editor === 'undefined') {
   loadSpinner();
 }
 
 // Check that service workers are supported
-if ("serviceWorker" in navigator) {
+if ('serviceWorker' in navigator) {
   // Use the window load event to keep the page load performant
-  window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/service-worker.js");
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js');
   });
 }
